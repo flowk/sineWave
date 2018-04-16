dd4fe97ec48c753f0ad0ad7068279f95907cf767
flowk <flo86@bluewin.ch>
2018-04-16 20:31:48 +0200

    final version
    so ists fast perfekt

--- sketch.js
+++ sketch.js
@@ -1,30 +1,46 @@
 let cnv;
-let radius = 50;
+let radius = 30;
+let forms = 10;
 let vertices = 5;
 let strk = 5;
+let inc;
+let wave = 0;
+let waveHeight = 100;
+let waveFreq = 0;
+let period = 3;
+let loop = 30;
+
 
 function setup() {
   // put setup code here
     cnv = createCanvas(windowWidth, windowHeight);
     cnv.parent('p5Sketch');
     //createCanvas(600,600, WEBGL);
-    frameRate(30);
+		frameRate(30);
+		inc = PI/30;
 }
 
 function draw() {
   // put drawing code here
   background(0);
 	noFill();
-	let strk = 	document.getElementById('stroke').value;
+	let period =	document.getElementById('period').value;
 
-	strokeWeight(strk);
+	strokeWeight(1);
 	stroke(128, 0, 128);
 
 	let value = document.getElementById('vertices').value;
+	let forms = document.getElementById('forms').value;
+	let waveHeight = document.getElementById('waveHeight').value;
+
+
 	vertices = value;
-	for(let i=0; i < 10; i++){
-		polygon(vertices, radius * i, width/2, height/2);
+	for(let i=0; i < forms; i++){
+		waveFreq = map(i, 0, loop, 0, TWO_PI * period);
+		let y = (height/2) + (sin(wave + waveFreq) * waveHeight);
+		polygon(vertices, radius * i/2, width/2, y);
 	}
+	wave += inc;
 }
 
 function windowResized() {
