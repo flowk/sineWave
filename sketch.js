89d929a5faad3d92902a9f5d519120674e977c5f
flowk <flo86@bluewin.ch>
2018-04-16 18:53:38 +0200

    minor changes

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-var OFF_MAX = 200;
 var counter = 0;
 
 function setup() {
   // put setup code here
-    //createCanvas(windowWidth, windowHeight);
-    createCanvas(600,600, WEBGL);
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    //createCanvas(600,600, WEBGL);
     //stroke(0);     // Set line drawing color to white
     frameRate(30);
 }
@@ -14,9 +13,7 @@ function draw() {
 
   background(0);
 
-
 }
-
 
 function windowResized() {
 	resizeCanvas(innerWidth, innerHeight);
