aebbab6d12ffb5c6e0ad5be86f02398782690ee4
flowk <flo86@bluewin.ch>
2018-04-16 19:59:18 +0200

    sketch fertig
    inkluseive zweitem slider für die linienstärke

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let cnv;
-var counter = 0;
+let radius = 50;
+let vertices = 5;
+let strk = 5;
 
 function setup() {
   // put setup code here
-    cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+    cnv = createCanvas(windowWidth, windowHeight);
     cnv.parent('p5Sketch');
     //createCanvas(600,600, WEBGL);
     frameRate(30);
@@ -12,9 +14,30 @@ function setup() {
 function draw() {
   // put drawing code here
   background(0);
+	noFill();
+	let strk = 	document.getElementById('stroke').value;
 
+	strokeWeight(strk);
+	stroke(128, 0, 128);
+
+	let value = document.getElementById('vertices').value;
+	vertices = value;
+	for(let i=0; i < 10; i++){
+		polygon(vertices, radius * i, width/2, height/2);
+	}
 }
 
 function windowResized() {
 	resizeCanvas(innerWidth, innerHeight);
+}
+
+function polygon(vert, r, posX, posY){
+	beginShape();
+	for(let i=0; i< vert; i++){
+		let angle = map(i, 0, vert, 0, TWO_PI);
+		let x = cos(angle) * r + posX;
+		let y = sin(angle) * (r/2) + posY;	
+		vertex(x, y);	
+	}
+	endShape(CLOSE);
 }
